da6bf16e84583034d2be47e32d53c5295eb938de
Matthew <matthew.thompson.a@gmail.com>
2016-05-14 13:00:31 -0400

    Initial commit to upload random number guess files

---
+++ random-number-guess.js
@@ -0,0 +1,22 @@
+var correctGuess = false;
+var randomNumber = Math.floor(Math.random()*10)+1;
+var guess = prompt("I am thinking of a number between 1 and 10. What is it?");
+
+if (parseInt(guess) === randomNumber) {
+  correctGuess = true;
+}
+  else if (parseInt(guess) < randomNumber) {
+    var guessMore= prompt("Try again. The number is more than " + guess);
+    if (parseInt(guessMore) === randomNumber) {correctGuess = true;}
+  }
+
+  else if (parseInt(guess) > randomNumber) {
+    var guessLess = prompt ("Try again. The number is less than " + guess);
+    if (parseInt(guessLess)===randomNumber) {correctGuess = true;}
+  }
+
+if (correctGuess){
+  document.write("<p> You guessed the number. Congrats!</p>");
+} else {
+  document.write ("<p> Sorry. The correct number was " + randomNumber + ".</p>");
+}
